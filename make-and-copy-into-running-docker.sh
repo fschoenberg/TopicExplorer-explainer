e151381ea30c5e2057908d7dd3100b0df2269161
Alexander Hinneburg <alexander.hinneburg@informatik.uni-halle.de>
2020-04-22 14:29:29 +0200

    update version of te docker image

--- make-and-copy-into-running-docker.sh
+++ make-and-copy-into-running-docker.sh
@@ -1,2 +1,2 @@
 elm make --output elm-te.js src/Main.elm
-sudo docker cp elm-te.js `sudo docker ps -q --filter ancestor=hinneburg/topicexplorer-docker:1.1.1`:/topicexplorer/html/explainer/
+sudo docker cp elm-te.js `sudo docker ps -q --filter ancestor=hinneburg/topicexplorer-docker:1.1.4`:/topicexplorer/html/explainer/
